75fd4fe65002bf27248f208989557b1f47643240
enrique <casillasenrique@live.com>
2024-02-21 02:11:44 -0500

    Optional contact form

--- global.js
+++ global.js
@@ -60,3 +60,17 @@ const select = document.querySelector('select');
 select.addEventListener('input', function (event) {
   setColorScheme(event.target.value);
 });
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const data = new FormData(form);
+  let url = 'mailto:example@example.com?';
+  for (let [name, value] of data) {
+    url = url + `${name}=${encodeURIComponent(value)}&`;
+    console.log(name, value);
+  }
+
+  location.href = url;
+});
